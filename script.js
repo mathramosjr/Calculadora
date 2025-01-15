74e52294c070788545d6314d50715dc91111147f
mathramosjr <math.ramosjr@gmail.com>
2025-01-15 03:31:06 -0300

    calculadora

---
+++ script.js
@@ -0,0 +1,49 @@
+let display = document.getElementById("display");
+let currentInput = ""
+let currentOperator = ""
+let calculated = false;
+
+//função para adicionar 1 numero a conta
+function appendNumber(value){
+    if(calculated === true){
+        currentInput = "";
+        calculated = false;
+    }
+    currentInput += value
+    display.textContent = currentInput
+}
+
+//função para adicionar um operador na conta
+function appendOperator(operator){
+    calculated = false;
+    if(currentInput === "" && operator !== ".") return
+    if(/[+\-*/.]$/.test(currentInput)) return;
+    currentInput += operator;
+    display.textContent = currentInput;
+}
+
+//função que calcula a conta
+function calculate(){
+    try{
+        let result = eval(currentInput)
+        currentInput = result;
+        display.textContent = currentInput;
+        calculated = true;
+    } catch(error){
+        display.textContent = "Error";
+        currentInput = "";
+    }
+}
+
+//função para apagar a conta
+function clearDisplay(){
+    currentInput = "";
+    display.textContent = 0;
+}
+
+//função para inverter a conta
+function invert(){
+    calculate();
+    currentInput = currentInput*-1;
+    display.textContent = currentInput;
+}
